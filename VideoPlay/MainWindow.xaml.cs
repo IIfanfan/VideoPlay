044d92ab9d49fd25bd8dbcfe6d69867fa220406d
TianLiangFan <1092924979@qq.com>
2021-04-23 15:51:10 +0800

    添加项目文件。

---
+++ VideoPlay/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using RP.ScoutRobot.WpfUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VideoPlay
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            VideoWindow videoWindow = new VideoWindow("视频播放", "F:/TestMp4.mp4");
+            videoWindow.ShowDialog();
+        }
+    }
+}
---
+++ VideoPlay/VideoWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace RP.ScoutRobot.WpfUI
+{
+    /// <summary>
+    /// VideoWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class VideoWindow
+    {
+        public VideoWindow(string title,string fileName, int width = 800, int height = 450)
+        {
+            InitializeComponent();
+            this.Title = title;
+            this.Width = width;
+            this.Height = height;
+
+            this.Left = (System.Windows.SystemParameters.PrimaryScreenWidth - width) / 2;
+            this.Top = (System.Windows.SystemParameters.PrimaryScreenHeight - height) / 2;
+            MediaPlayer.Source = new Uri(fileName);
+        }
+        private void ButtonPlay_Click(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer.Play();
+        }
+
+        private void ButtonStop_Click(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer.Stop();
+        }
+
+        private void ButtonForward_Click(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer.Position = MediaPlayer.Position + TimeSpan.FromSeconds(20);
+        }
+
+        private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        {
+            MediaPlayer.Position = MediaPlayer.Position - TimeSpan.FromSeconds(20);
+        }
+        DispatcherTimer timer = null;
+        private void MediaPlayer_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            // Get the lenght of the video
+            sliderFontSize.Maximum = MediaPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+            //媒体文件打开成功
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += new EventHandler(timer_tick);
+            timer.Start();
+        }
+        private void timer_tick(object sender, EventArgs e)
+        {
+            sliderFontSize.Value = MediaPlayer.Position.TotalSeconds;
+        }
+
+        private void sliderFontSize_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            MediaPlayer.Position = TimeSpan.FromSeconds(sliderFontSize.Value);
+        }
+    }
+}
